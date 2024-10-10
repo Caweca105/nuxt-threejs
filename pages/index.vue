41837dc976913de977ce2af4f1e32a5c903f9efe
Ricardo Justino <ricardo.carvalho.justino@pwc.com>
2024-10-10 01:27:02 +0100

    Add initial project files and configurations

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page">
+    <ClientOnly><TheExperience /></ClientOnly>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
